186afc4d88f42208d57d2d8ba58e9f58798abc72
Adam Shin <adamshindev@gmail.com>
2025-04-15 14:14:27 -0600

    Navbar links

---
+++ src/components/navbar/NavLinks.ts
@@ -0,0 +1,8 @@
+
+export const navLinks = [
+  { href: "/about", text: "About Adam" },
+  { href: "/issues", text: "Issues" },
+  { href: "/get-involved", text: "Get Involved" },
+  { href: "/contact", text: "Contact" },
+  { href: "/donate", text: "Donate" },
+];
